a2018f98256a671557c2a385213409e60b564feb
dmitriy_kochevanov <dima.kochevanov1984@gmail.com>
2024-05-13 21:32:02 +0300

    add arguments in main.py and fixed errors

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.widget import get_mask_account_card, get_new_date
+
+print(get_mask_account_card("Visa Platinum 8990922113665229"))
+
+print(get_mask_account_card("Счет 73654108430135874305"))
+
+print(get_new_date("2018-07-11T02:26:18.671407"))
--- src/widget.py
+++ src/widget.py
@@ -1,19 +1,21 @@
-from src.masks import get_mask_account,get_mask_card
+from src.masks import get_mask_account, get_mask_card
+
+
 def get_mask_account_card(number: str) -> str:
-    """ Функция принимает строку и выдает замаскированный
-    номер карты или счета """
+    """Функция принимает строку и выдает замаскированный
+    номер карты или счета"""
     if len(number.split()[-1]) == 16:
         mask_card = get_mask_card(number.split()[-1])
-        result = f"{number[:-16]}{mask_card}"
-        return result
+        result_mask_card = f"{number[:-16]}{mask_card}"
+        return result_mask_card
     elif len(number.split()[-1]) == 20:
         mask_account = get_mask_account(number.split()[-1])
-        result = f"{number[:-20]}{mask_account}"
-        return result
+        result_mask_account = f"{number[:-20]}{mask_account}"
+    return result_mask_account
 
 
-def get_new_date(old_date: str) ->str:
-    """ Функция принимает строку с датой и преобразует ее в более
-    читаемый вид """
+def get_new_date(old_date: str) -> str:
+    """Функция принимает строку с датой и преобразует ее в более
+    читаемый вид"""
     date_slise = old_date[0:10].split("-")
     return ".".join(date_slise[::-1])
