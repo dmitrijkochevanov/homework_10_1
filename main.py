987e93e810765626fe31f614f8a3208abfb67d3f
dmitriy_kochevanov <dima.kochevanov1984@gmail.com>
2024-05-18 15:05:26 +0200

    add funcs get_dictionary_key and get_date_sorted for processing.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from src.widget import get_mask_account_card, get_new_date
+from src.processing import get_dictionary_key,get_date_sorted
 
 print(get_mask_account_card("Visa Platinum 8990922113665229"))
 
 print(get_mask_account_card("Счет 73654108430135874305"))
 
 print(get_new_date("2018-07-11T02:26:18.671407"))
+
+print(get_dictionary_key([{'id': "41428829", 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                          {'id': "939719570", 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                          {'id': "594226727", 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                          {'id': "615064591", 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+
+print(get_date_sorted([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                          {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                          {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                          {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
